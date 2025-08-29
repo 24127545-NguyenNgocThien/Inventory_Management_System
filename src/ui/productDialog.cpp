6a54b589f2c7ad1b6ea32fb59fdfa827909b63ec
Nguyen Thien <thien217353@gmail.com>
2025-08-29 12:24:40 +0700

    completed feature invoice

--- src/ui/productDialog.cpp
+++ src/ui/productDialog.cpp
@@ -1,371 +1,3 @@
-// /*
-// #include "productDialog.h"
-// #include <QFormLayout>
-// #include <QHBoxLayout>
-// #include <QDialogButtonBox>
-
-// ProductDialog::ProductDialog(QWidget *parent)
-//     : QDialog(parent)
-// {
-//     setWindowTitle("Thông tin sản phẩm");
-
-//     categoryBox = new QComboBox(this);
-//     categoryBox->addItems({"Điện", "Nước", "Khác"});
-//     nameEdit = new QLineEdit(this);
-//     brandEdit = new QLineEdit(this);
-//     importPriceSpin = new QDoubleSpinBox(this);
-//     importPriceSpin->setMaximum(1e9);
-//     salePriceSpin = new QDoubleSpinBox(this);
-//     salePriceSpin->setMaximum(1e9);
-//     quantitySpin = new QSpinBox(this);
-//     quantitySpin->setMaximum(1000000);
-
-//     // Layout form chính
-//     QFormLayout *formLayout = new QFormLayout;
-//     formLayout->addRow("Loại:", categoryBox);
-//     formLayout->addRow("Tên:", nameEdit);
-//     formLayout->addRow("Tên:", brandEdit);
-//     formLayout->addRow("Giá nhập:", importPriceSpin);
-//     formLayout->addRow("Giá bán:", salePriceSpin);
-//     formLayout->addRow("Số lượng:", quantitySpin);
-
-//     // Gọi ngay để hiển thị đúng "Điện" lúc mở
-//     onCategoryChanged(categoryBox->currentIndex());
-
-//     // Extra fields container
-//     extraWidget = new QWidget(this);
-//     extraLayout = new QFormLayout(extraWidget);
-//     formLayout->addRow(extraWidget);
-
-//     // Buttons
-//     QDialogButtonBox *buttonBox =
-//         new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
-//     connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
-//     connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
-
-//     // Layout chính
-//     QVBoxLayout *mainLayout = new QVBoxLayout(this);
-//     mainLayout->addLayout(formLayout);
-//     mainLayout->addWidget(buttonBox);
-//     setLayout(mainLayout);
-
-//     connect(categoryBox, QOverload<int>::of(&QComboBox::currentIndexChanged),
-//             this, &ProductDialog::onCategoryChanged);
-// }
-
-// QString ProductDialog::getName() const
-// {
-//     return nameEdit->text();
-// }
-
-// QString ProductDialog::getBrand() const
-// {
-//     return brandEdit->text();
-// }
-
-// int ProductDialog::getCategory() const
-// {
-//     return categoryBox->currentIndex();
-// }
-
-// double ProductDialog::getImportPrice() const
-// {
-//     return importPriceSpin->value();
-// }
-
-// double ProductDialog::getSalePrice() const
-// {
-//     return salePriceSpin->value();
-// }
-
-// int ProductDialog::getQuantity() const
-// {
-//     return quantitySpin->value();
-// }
-
-// QMap<QString, QString> ProductDialog::getExtraFields() const
-// {
-//     QMap<QString, QString> fields;
-//     QList<QLineEdit*> edits = extraWidget->findChildren<QLineEdit*>();
-//     for (QLineEdit *edit : edits) {
-//         fields.insert(edit->objectName(), edit->text());
-//     }
-//     return fields;
-// }
-
-// void ProductDialog::onCategoryChanged(int index) {
-//     // Xóa field cũ
-//     QLayoutItem *item;
-//     while ((item = extraLayout->takeAt(0)) != nullptr) {
-//         if (item->widget()) item->widget()->deleteLater();
-//         delete item;
-//     }
-
-//     if (index == 0) { // Điện
-//         QLineEdit *safety = new QLineEdit(this);
-//         safety->setObjectName("extra1");
-//         QLineEdit *power = new QLineEdit(this);
-//         power->setObjectName("extra2");
-//         QLineEdit *voltage = new QLineEdit(this);
-//         voltage->setObjectName("extra3");
-
-//         extraLayout->addRow("Tiêu chuẩn an toàn:", safety);
-//         extraLayout->addRow("Công suất (W):", power);
-//         extraLayout->addRow("Điện áp (V):", voltage);
-//     }
-//     else if (index == 1) { // Nước
-//         QLineEdit *material = new QLineEdit(this);
-//         material->setObjectName("extra1");
-//         QLineEdit *temperatureLimit = new QLineEdit(this);
-//         temperatureLimit->setObjectName("extra2");
-
-//         extraLayout->addRow("Chất liệu:", material);
-//         extraLayout->addRow("Độ chịu nhiệt:", temperatureLimit);
-//     }
-//     // index == 2 (Khác) => để trống
-// }
-// */
-
-// #include "ProductDialog.h"
-// #include <QVBoxLayout>
-// #include <QDebug>
-// #include <QSignalBlocker>
-
-// ProductDialog::ProductDialog(QWidget *parent)
-//     : QDialog(parent)
-// {
-//     setWindowTitle("Thêm/Sửa sản phẩm");
-//     setModal(true);
-
-//     formLayout = new QFormLayout;
-
-//     // --- Category ---
-//     categoryBox = new QComboBox(this);
-//     categoryBox->addItems({"Điện", "Nước", "Khác"});
-//     connect(categoryBox, &QComboBox::currentIndexChanged, this, &ProductDialog::onCategoryChanged);
-
-//     // --- Các trường cơ bản ---
-//     nameEdit = new QLineEdit(this);
-//     brandEdit = new QLineEdit(this);
-
-//     importPriceSpin = new QDoubleSpinBox(this);
-//     importPriceSpin->setRange(0, 1e9);
-//     importPriceSpin->setDecimals(2);
-//     importPriceSpin->setSingleStep(1000.0);
-
-//     salePriceSpin = new QDoubleSpinBox(this);
-//     salePriceSpin->setRange(0, 1e9);
-//     salePriceSpin->setDecimals(2);
-//     salePriceSpin->setSingleStep(1000.0);
-
-//     quantitySpin = new QSpinBox(this);
-//     quantitySpin->setRange(0, 1e9);
-
-//     // --- Thêm vào form layout ---
-//     formLayout->addRow("Loại:", categoryBox);
-//     formLayout->addRow("Tên:", nameEdit);
-//     formLayout->addRow("Thương hiệu:", brandEdit);
-//     formLayout->addRow("Giá nhập:", importPriceSpin);
-//     formLayout->addRow("Giá bán:", salePriceSpin);
-//     formLayout->addRow("Số lượng:", quantitySpin);
-
-//     // --- ButtonBox (OK/Cancel) ---
-//     QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
-//     connect(buttonBox, &QDialogButtonBox::accepted, this, &ProductDialog::accept);
-//     connect(buttonBox, &QDialogButtonBox::rejected, this, &ProductDialog::reject);
-
-//     QVBoxLayout *mainLayout = new QVBoxLayout;
-//     mainLayout->addLayout(formLayout);
-//     mainLayout->addWidget(buttonBox);
-
-//     setLayout(mainLayout);
-
-//     // Gọi thủ công khi mở lần đầu -> đảm bảo extras được tạo
-//     onCategoryChanged(categoryBox->currentIndex());
-// }
-
-// ProductDialog::~ProductDialog()
-// {
-//     clearExtras();
-// }
-
-// /**
-//  * clearExtras:
-//  *  - Xóa an toàn cả widget (ví dụ QLineEdit) và QLabel tương ứng
-//  *  - Lý do: formLayout->addRow("Label", widget) tạo thêm một QLabel cho Label; nếu
-//  *    chỉ delete widget thì label vẫn còn trong layout => gây lỗi logic hiển thị.
-//  */
-// void ProductDialog::clearExtras() {
-//     // // Xóa tất cả field bổ sung trước đó
-//     // for (QLineEdit *w : extras) {
-//     //     // lấy label gắn với field này
-//     //     QWidget *labelWidget = formLayout->labelForField(w);
-//     //     if (labelWidget) {
-//     //         QLabel *lbl = qobject_cast<QLabel*>(labelWidget); // Ép kiểu an toàn
-//     //         if (lbl) {
-//     //             formLayout->removeWidget(lbl);
-//     //             lbl->deleteLater();
-//     //         }
-//     //     }
-//     //     formLayout->removeWidget(w);
-//     //     w->deleteLater();
-//     // }
-//     // extras.clear();
-//     // duyệt tất cả các item từ dưới lên
-//     // for (int i = formLayout->rowCount() - 1; i >= 0; --i) {
-//     //     QWidget *field = formLayout->itemAt(i, QFormLayout::FieldRole) ?
-//     //                          formLayout->itemAt(i, QFormLayout::FieldRole)->widget() : nullptr;
-//     //     QWidget *label = formLayout->itemAt(i, QFormLayout::LabelRole) ?
-//     //                          formLayout->itemAt(i, QFormLayout::LabelRole)->widget() : nullptr;
-
-//     //     if (field && extras.values().contains(static_cast<QLineEdit*>(field))) {
-//     //         formLayout->removeRow(i);
-//     //         field->deleteLater();
-//     //         if (label) label->deleteLater();
-//     //     }
-//     // }
-//     // extras.clear();
-//     for (auto it = extras.begin(); it != extras.end(); ++it) {
-//         QWidget *field = it.value();
-//         if (!field) continue;
-
-//         // Xóa label đi kèm field này
-//         QWidget *label = formLayout->labelForField(field);
-//         if (label) {
-//             formLayout->removeWidget(label);
-//             label->deleteLater();
-//         }
-
-//         // Xóa chính field
-//         formLayout->removeWidget(field);
-//         field->deleteLater();
-//     }
-//     extras.clear();
-// }
-
-// void ProductDialog::onCategoryChanged(int index)
-// {
-//     // // Trước hết xóa extras cũ (bao gồm label)
-//     // clearExtras();
-
-//     // // Tạo extra fields tùy loại
-//     // if (index == 0) { // Điện -> 3 extra
-//     //     QLineEdit *extra1 = new QLineEdit(this);
-//     //     QLineEdit *extra2 = new QLineEdit(this);
-//     //     QLineEdit *extra3 = new QLineEdit(this);
-//     //     extra1->setObjectName("extra1");
-//     //     extra2->setObjectName("extra2");
-//     //     extra3->setObjectName("extra3");
-//     //     extras["extra1"] = extra1;
-//     //     extras["extra2"] = extra2;
-//     //     extras["extra3"] = extra3;
-//     //     formLayout->addRow("Tiêu chuẩn an toàn: ", extra1);
-//     //     formLayout->addRow("Điện năng (W): ", extra2);
-//     //     formLayout->addRow("Vôn (V): ", extra3);
-//     // } else if (index == 1) { // Nước -> 2 extra
-//     //     QLineEdit *extra1 = new QLineEdit(this);
-//     //     QLineEdit *extra2 = new QLineEdit(this);
-//     //     extra1->setObjectName("extra1");
-//     //     extra2->setObjectName("extra2");
-//     //     extras["extra1"] = extra1;
-//     //     extras["extra2"] = extra2;
-//     //     formLayout->addRow("Chất liệu: ", extra1);
-//     //     formLayout->addRow("Độ chịu nhiệt (C): ", extra2);
-//     // } else {
-//     //     // Khác: không có extra
-//     // }
-//     clearExtras(); // luôn xóa sạch trước
-
-//     if (index == 0) { // Điện
-//         auto e1 = new QLineEdit(this);
-//         auto e2 = new QLineEdit(this);
-//         auto e3 = new QLineEdit(this);
-//         formLayout->addRow("Tiêu chuẩn an toàn:", e1);
-//         formLayout->addRow("Điện năng (W):", e2);
-//         formLayout->addRow("Vôn (V):", e3);
-//         extras["extra1"] = e1;
-//         extras["extra2"] = e2;
-//         extras["extra3"] = e3;
-//     }
-//     else if (index == 1) { // Nước
-//         auto e1 = new QLineEdit(this);
-//         auto e2 = new QLineEdit(this);
-//         formLayout->addRow("Chất liệu:", e1);
-//         formLayout->addRow("Dung tích (L):", e2);
-//         extras["extra1"] = e1;
-//         extras["extra2"] = e2;
-//     }
-//     else {
-//         // loại "Khác" -> không có extras
-//     }
-// }
-
-// /*** Getters ***/
-// QString ProductDialog::getName() const {
-//     return nameEdit->text();
-// }
-
-// QString ProductDialog::getBrand() const {
-//     return brandEdit->text();
-// }
-
-// int ProductDialog::getCategory() const {
-//     return categoryBox->currentIndex();
-// }
-
-// double ProductDialog::getImportPrice() const {
-//     return importPriceSpin->value();
-// }
-
-// double ProductDialog::getSalePrice() const {
-//     return salePriceSpin->value();
-// }
-
-// int ProductDialog::getQuantity() const {
-//     return quantitySpin->value();
-// }
-
-// QMap<QString, QString> ProductDialog::getExtras() const {
-//     QMap<QString, QString> result;
-//     for (auto it = extras.begin(); it != extras.end(); ++it) {
-//         if (it.value())
-//             result[it.key()] = it.value()->text();
-//     }
-//     return result;
-// }
-
-// /*** Helpers cho Edit mode ***/
-// void ProductDialog::setCategoryLocked(bool locked)
-// {
-//     categoryBox->setEnabled(!locked);
-// }
-
-// void ProductDialog::setProductData(const QString &name,
-//                                    const QString &brand,
-//                                    double importPrice,
-//                                    double salePrice,
-//                                    int quantity,
-//                                    int categoryIndex,
-//                                    const QMap<QString, QString> &extraValues,
-//                                    bool lockCategory)
-// {
-//     nameEdit->setText(name);
-//     brandEdit->setText(brand);
-//     importPriceSpin->setValue(importPrice);
-//     salePriceSpin->setValue(salePrice);
-//     quantitySpin->setValue(quantity);
-
-//     // Gọi explicit để đảm bảo extras đã tạo
-//     onCategoryChanged(categoryIndex);
-
-//     // Gán giá trị extra nếu có
-//     for (auto it = extraValues.begin(); it != extraValues.end(); ++it) {
-//         if (extras.contains(it.key()) && extras[it.key()])
-//             extras[it.key()]->setText(it.value());
-//     }
-
-//     setCategoryLocked(lockCategory);
-// }
 #include "productDialog.h"
 #include <QVBoxLayout>
 #include <QLabel>
@@ -431,6 +63,11 @@ ProductDialog::~ProductDialog() {
     clearExtras();
 }
 
+
+
+
+
+
 void ProductDialog::addExtraLine(const QString &label, const QString &key) {
     auto *e = new QLineEdit(this);
     extrasEdits.insert(key, e);
@@ -439,15 +76,19 @@ void ProductDialog::addExtraLine(const QString &label, const QString &key) {
 
 // Xóa SẠCH vùng extras mà không đụng vào form chính
 void ProductDialog::clearExtras() {
+    // for (int i = extrasForm->rowCount() - 1; i >= 0; --i) {
+    //     if (auto *li = extrasForm->itemAt(i, QFormLayout::LabelRole)) {
+    //         if (auto *w = li->widget()) { extrasForm->removeWidget(w); w->deleteLater(); }
+    //         else delete li;
+    //     }
+    //     if (auto *fi = extrasForm->itemAt(i, QFormLayout::FieldRole)) {
+    //         if (auto *w = fi->widget()) { extrasForm->removeWidget(w); w->deleteLater(); }
+    //         else delete fi;
+    //     }
+    // }
+    // extrasEdits.clear();
     for (int i = extrasForm->rowCount() - 1; i >= 0; --i) {
-        if (auto *li = extrasForm->itemAt(i, QFormLayout::LabelRole)) {
-            if (auto *w = li->widget()) { extrasForm->removeWidget(w); w->deleteLater(); }
-            else delete li;
-        }
-        if (auto *fi = extrasForm->itemAt(i, QFormLayout::FieldRole)) {
-            if (auto *w = fi->widget()) { extrasForm->removeWidget(w); w->deleteLater(); }
-            else delete fi;
-        }
+        extrasForm->removeRow(i);   // gỡ cả label và field
     }
     extrasEdits.clear();
 }
@@ -463,7 +104,7 @@ void ProductDialog::onCategoryChanged(int index) {
         break;
     case 1: // Nước
         addExtraLine("Chất liệu",    "extra1");
-        addExtraLine("Dung tích (L)","extra2");
+        addExtraLine("Độ chịu nhiệt (C)","extra2");
         break;
     case 2: // Khác
     default:
@@ -472,6 +113,11 @@ void ProductDialog::onCategoryChanged(int index) {
     }
 }
 
+
+
+
+
+
 // --- Getters ---
 int     ProductDialog::getCategory() const    { return categoryBox->currentIndex(); }
 QString ProductDialog::getName() const        { return nameEdit->text(); }
@@ -479,7 +125,6 @@ QString ProductDialog::getBrand() const       { return brandEdit->text(); }
 double  ProductDialog::getImportPrice() const { return importPriceSpin->value(); }
 double  ProductDialog::getSalePrice() const   { return salePriceSpin->value(); }
 int     ProductDialog::getQuantity() const    { return quantitySpin->value(); }
-
 QMap<QString, QString> ProductDialog::getExtras() const {
     QMap<QString, QString> out;
     for (auto it = extrasEdits.constBegin(); it != extrasEdits.constEnd(); ++it) {
@@ -487,6 +132,11 @@ QMap<QString, QString> ProductDialog::getExtras() const {
     }
     return out;
 }
+
+
+
+
+
 
 void ProductDialog::setCategoryLocked(bool locked) {
     categoryBox->setEnabled(!locked);
@@ -509,12 +159,11 @@ void ProductDialog::setProductData(const QString &name,
 
     // Đặt loại -> Qt tự emit signal gọi onCategoryChanged để dựng lại extras
     categoryBox->setCurrentIndex(categoryIndex);
+    setCategoryLocked(lockCategory); // khóa loại khi edit
 
     // Gán giá trị extras
     for (auto it = extraValues.constBegin(); it != extraValues.constEnd(); ++it) {
         if (extrasEdits.contains(it.key()) && extrasEdits[it.key()])
             extrasEdits[it.key()]->setText(it.value());
     }
-
-    setCategoryLocked(lockCategory); // khóa loại khi edit nếu cần
 }
---
+++ src/ui/selectionDialog.cpp
@@ -0,0 +1,265 @@
+#include "selectiondialog.h"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QHeaderView>
+#include <QLabel>
+#include <QSpinBox>
+#include <QSet>
+
+SelectionDialog::SelectionDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    setWindowTitle("Chọn sản phẩm");
+    resize(900, 520);
+
+    // Filter widgets
+    comboType  = new QComboBox(this);
+    comboName  = new QComboBox(this);
+    comboBrand = new QComboBox(this);
+    editSearch = new QLineEdit(this);
+    editSearch->setPlaceholderText("Tìm nhanh theo tên / id...");
+
+    comboType->addItem("Tất cả");
+    comboName->addItem("Tất cả");
+    comboBrand->addItem("Tất cả");
+
+    // Layout filter (gọn gàng trong 1 group)
+    QHBoxLayout *hl = new QHBoxLayout;
+    hl->addWidget(new QLabel("Loại:"));
+    hl->addWidget(comboType, 1);
+    hl->addSpacing(8);
+    hl->addWidget(new QLabel("Tên:"));
+    hl->addWidget(comboName, 2);
+    hl->addSpacing(8);
+    hl->addWidget(new QLabel("Thương hiệu:"));
+    hl->addWidget(comboBrand, 2);
+    hl->addSpacing(12);
+    hl->addWidget(new QLabel("Tìm:"));
+    hl->addWidget(editSearch, 3);
+
+    // Table
+    table = new QTableWidget(this);
+    table->setColumnCount(7);
+    table->setHorizontalHeaderLabels({"Mã", "Tên", "Thương hiệu", "Giá nhập", "Giá bán", "Tồn kho", "Số lượng"});
+    table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+    table->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+    table->horizontalHeader()->setSectionResizeMode(2, QHeaderView::ResizeToContents);
+    table->horizontalHeader()->setSectionResizeMode(3, QHeaderView::ResizeToContents);
+    table->horizontalHeader()->setSectionResizeMode(4, QHeaderView::ResizeToContents);
+    table->horizontalHeader()->setSectionResizeMode(5, QHeaderView::ResizeToContents);
+    table->horizontalHeader()->setSectionResizeMode(6, QHeaderView::ResizeToContents);
+    table->setSelectionBehavior(QAbstractItemView::SelectRows);
+    table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    table->setAlternatingRowColors(true);
+    table->verticalHeader()->setVisible(false);
+
+    // Buttons
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &SelectionDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &SelectionDialog::reject);
+
+    // Main layout
+    QVBoxLayout *main = new QVBoxLayout(this);
+    main->addLayout(hl);
+    main->addWidget(table);
+    main->addWidget(buttonBox);
+    setLayout(main);
+
+    // Signals
+    connect(comboType,  QOverload<int>::of(&QComboBox::currentIndexChanged), this, &SelectionDialog::onTypeChanged);
+    connect(comboName,  QOverload<int>::of(&QComboBox::currentIndexChanged), this, &SelectionDialog::onNameChanged);
+    connect(comboBrand, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &SelectionDialog::onBrandChanged);
+    connect(editSearch, &QLineEdit::textChanged, this, &SelectionDialog::onSearchTextChanged);
+}
+
+void SelectionDialog::loadData(const QList<SelectedProduct> &products)
+{
+    // copy toàn bộ dữ liệu nguồn
+    allProducts = products;
+
+    // build các combo theo toàn bộ tập dữ liệu
+    rebuildFilterCombosFrom(allProducts);
+
+    // áp dụng filter ban đầu
+    applyFilters();
+}
+
+void SelectionDialog::rebuildFilterCombosFrom(const QList<SelectedProduct> &source)
+{
+    // Tạo danh sách unique cho mỗi filter
+    QSet<QString> types;
+    QSet<QString> names;
+    QSet<QString> brands;
+    for (const auto &p : source) {
+        if (!p.type.isEmpty())  types.insert(p.type);
+        if (!p.name.isEmpty())  names.insert(p.name);
+        if (!p.brand.isEmpty()) brands.insert(p.brand);
+    }
+
+    // Block signals khi rebuild để tránh applyFilters nhiều lần
+    comboType->blockSignals(true);
+    comboName->blockSignals(true);
+    comboBrand->blockSignals(true);
+
+    comboType->clear();  comboType->addItem("Tất cả");
+    for (const QString &t : types) comboType->addItem(t);
+
+    comboName->clear();  comboName->addItem("Tất cả");
+    for (const QString &n : names) comboName->addItem(n);
+
+    comboBrand->clear(); comboBrand->addItem("Tất cả");
+    for (const QString &b : brands) comboBrand->addItem(b);
+
+    comboType->blockSignals(false);
+    comboName->blockSignals(false);
+    comboBrand->blockSignals(false);
+}
+
+void SelectionDialog::applyFilters()
+{
+    filteredProducts.clear();
+
+    QString selType  = comboType->currentText();
+    QString selName  = comboName->currentText();
+    QString selBrand = comboBrand->currentText();
+    QString search   = editSearch->text().trimmed();
+
+    for (const auto &p : allProducts) {
+        if (selType != "Tất cả" && !p.type.compare(selType, Qt::CaseInsensitive) == 0) {
+            // note: use exact match for type; if type stored differently adapt accordingly
+        }
+        // We'll do comparisons in a simpler way to be case-insensitive and flexible:
+        bool ok = true;
+        if (selType != "Tất cả" && !p.type.compare(selType, Qt::CaseInsensitive) == 0) {
+            ok = false;
+        }
+        if (ok && selName != "Tất cả" && !p.name.compare(selName, Qt::CaseInsensitive) == 0) {
+            ok = false;
+        }
+        if (ok && selBrand != "Tất cả" && !p.brand.compare(selBrand, Qt::CaseInsensitive) == 0) {
+            ok = false;
+        }
+        if (!ok) continue;
+
+        // Search text filter: match id or name or brand (contains)
+        if (!search.isEmpty()) {
+            QString s = search.toLower();
+            if (!(p.id.toLower().contains(s) || p.name.toLower().contains(s) || p.brand.toLower().contains(s))) {
+                continue;
+            }
+        }
+
+        filteredProducts.append(p);
+    }
+
+    // Update dependent combo boxes based on current filteredProducts:
+    // Allow narrowing: if user picked Type, we want Name/Brand lists based on that type.
+    // We'll compute available names/brands from the currently filteredProducts BEFORE search (so search shouldn't clear combos)
+    // For simplicity, rebuild names/brands from filteredProducts:
+    QSet<QString> namesSet, brandsSet;
+    for (const auto &fp : filteredProducts) {
+        namesSet.insert(fp.name);
+        brandsSet.insert(fp.brand);
+    }
+
+    comboName->blockSignals(true);
+    comboBrand->blockSignals(true);
+
+    // Keep current selection if still present, otherwise reset to "Tất cả"
+    QString curName  = comboName->currentText();
+    QString curBrand = comboBrand->currentText();
+
+    comboName->clear();
+    comboName->addItem("Tất cả");
+    for (const QString &n : namesSet) comboName->addItem(n);
+
+    comboBrand->clear();
+    comboBrand->addItem("Tất cả");
+    for (const QString &b : brandsSet) comboBrand->addItem(b);
+
+    // restore selection if it still exists
+    int nameIndex = comboName->findText(curName);
+    if (nameIndex >= 0) comboName->setCurrentIndex(nameIndex); // keep previous
+    int brandIndex = comboBrand->findText(curBrand);
+    if (brandIndex >= 0) comboBrand->setCurrentIndex(brandIndex);
+
+    comboName->blockSignals(false);
+    comboBrand->blockSignals(false);
+
+    populateTable();
+}
+
+void SelectionDialog::populateTable()
+{
+    table->setRowCount(filteredProducts.size());
+    for (int i = 0; i < filteredProducts.size(); ++i) {
+        const SelectedProduct &p = filteredProducts.at(i);
+
+        // ID
+        QTableWidgetItem *itId = new QTableWidgetItem(p.id);
+        itId->setData(Qt::UserRole, p.id);
+        table->setItem(i, 0, itId);
+
+        // Name
+        table->setItem(i, 1, new QTableWidgetItem(p.name));
+        // Brand
+        table->setItem(i, 2, new QTableWidgetItem(p.brand));
+        // Import price
+        table->setItem(i, 3, new QTableWidgetItem(QString::number(p.importPrice)));
+        // Sale price
+        table->setItem(i, 4, new QTableWidgetItem(QString::number(p.salePrice)));
+        // Stock
+        table->setItem(i, 5, new QTableWidgetItem(QString::number(p.stock)));
+
+        // Quantity - use QSpinBox (0..stock)
+        QSpinBox *spin = new QSpinBox(this);
+        spin->setRange(0, qMax(0, p.stock));
+        spin->setValue(p.quantity); // if had previous selection
+        spin->setMinimumWidth(80);
+        table->setCellWidget(i, 6, spin);
+    }
+
+    // Optionally adjust row heights
+    table->verticalHeader()->setDefaultSectionSize(28);
+}
+
+QList<SelectedProduct> SelectionDialog::getSelections() const
+{
+    QList<SelectedProduct> out;
+    for (int i = 0; i < table->rowCount(); ++i) {
+        QWidget *w = table->cellWidget(i, 6);
+        QSpinBox *spin = qobject_cast<QSpinBox*>(w);
+        int qty = spin ? spin->value() : 0;
+        if (qty <= 0) continue; // only return selected > 0
+
+        SelectedProduct p;
+        p.id = table->item(i, 0)->text();
+        p.name = table->item(i, 1)->text();
+        p.brand = table->item(i, 2)->text();
+        p.importPrice = table->item(i, 3)->text().toDouble();
+        p.salePrice = table->item(i, 4)->text().toDouble();
+        p.stock = table->item(i, 5)->text().toInt();
+        p.quantity = qty;
+        out.append(p);
+    }
+    return out;
+}
+
+/*** Slots ***/
+void SelectionDialog::onTypeChanged(int /*idx*/)
+{
+    applyFilters();
+}
+
+void SelectionDialog::onNameChanged(int /*idx*/)
+{
+    applyFilters();
+}
+void SelectionDialog::onBrandChanged(int /*idx*/)
+{
+    applyFilters();
+}
+void SelectionDialog::onSearchTextChanged(const QString &)
+{
+    applyFilters();
+}
